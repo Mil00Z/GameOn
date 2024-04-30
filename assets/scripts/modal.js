43ab0f7e95ed83c09db8e24e83f2601b8a0a0a4a
aladin002dz <mahfoudh.arous@gmail.com>
2024-04-30 14:50:18 +0100

    remove error message on change

--- assets/scripts/modal.js
+++ assets/scripts/modal.js
@@ -9,29 +9,29 @@ const modalCloser = document.querySelectorAll('.closer');
 //Get Modal In/Out by click trigger
 if (modalTrigger !== null && modalCloser !== null) {
 
-    modalTrigger.forEach((trigger) => {
+  modalTrigger.forEach((trigger) => {
 
-      // Launch modal event on single Click
-      trigger.addEventListener('click',(e)=> {
+    // Launch modal event on single Click
+    trigger.addEventListener('click', (e) => {
 
-          setDisplayModal(modalElement,'block');
-        
-      });
-
-      
-    });
-
-    modalCloser.forEach((closer) => {
-
-      closer.addEventListener('click',(e) =>{
-
-    
-        setDisplayModal(modalElement,'none');
-        switchStateElement(closer);
-
-      });
+      setDisplayModal(modalElement, 'block');
 
     });
+
+
+  });
+
+  modalCloser.forEach((closer) => {
+
+    closer.addEventListener('click', (e) => {
+
+
+      setDisplayModal(modalElement, 'none');
+      switchStateElement(closer);
+
+    });
+
+  });
 
 } else {
 
@@ -49,7 +49,7 @@ modalbodyChilds.forEach((modalChild) => {
 
   let getClassName = modalChild.getAttribute('class');
 
-  modalChild.dataset.display = getClassName; 
+  modalChild.dataset.display = getClassName;
 
   //Display Childs of the Form Body
   // console.log(modalChild.dataset.display);
@@ -59,16 +59,16 @@ modalbodyChilds.forEach((modalChild) => {
 // Testing Simple click "fake" submiting Form
 const formSubmit = document.getElementById('submiter');
 
-formSubmit.addEventListener('click',(e)=> {
+formSubmit.addEventListener('click', (e) => {
 
   e.preventDefault();
- 
+
   getDataInput('.modal-form');
   testSubmit();
 
- 
+
   e.target.closest('.modal-form').classList.toggle('sucess');
-  
+
   document.body.querySelector('.modal-message').classList.toggle('on');
 
 });
@@ -83,7 +83,7 @@ const firstInput = document.querySelector('#first');
 //       console.error(`${firstInput.getAttribute('name')} - data input is not correct`);
 
 //    } else {
-    
+
 //     console.log(`Goods values '${firstInput.value}' for ${firstInput}`);
 //     // prompt(`Goods values for ${firstInput.value}`);
 
@@ -99,9 +99,9 @@ const lastInput = document.querySelector('#last');
 //      console.error(`${lastInput.getAttribute('name')} - data input is not correct`);
 
 //   } else {
-   
+
 //    console.log(`Goods values '${lastInput.value}' for ${lastInput}`)
-   
+
 
 //   }
 
@@ -110,14 +110,15 @@ const lastInput = document.querySelector('#last');
 //Inputs Names : Testing group of same datas Text
 const namesInput = document.querySelectorAll('#first,#last');
 
-namesInput.forEach((input)=>{
+namesInput.forEach((input) => {
 
-  input.addEventListener('change',(e) =>{
+  input.addEventListener('change', (e) => {
 
-    if(input.value.length < 2 || input.value == ''){
+    const parentFormData = input.closest('.formData');
+    if (input.value.length < 2 || input.value == '') {
 
       // Display Infos in Log
-      displayInputDataLog(input,'wrong');
+      displayInputDataLog(input, 'wrong');
 
       //Inject Warning Message about the Input Field
       let newWarning = document.createElement('div');
@@ -126,7 +127,7 @@ namesInput.forEach((input)=>{
       newWarning.textContent = `❌ "${input.value}" is too short.
       2 Characters required`;
 
-      input.closest('.formData').append(newWarning);
+      parentFormData.append(newWarning);
 
     } else {
 
@@ -135,9 +136,7 @@ namesInput.forEach((input)=>{
 
       // Testing Element on DOm + Remove Warning
       console.log(input.closest('.formData .debug-input'));
-      input.closest('.formData .debug-input').remove();
-
-  
+      parentFormData.querySelector('.debug-input').remove();
     }
 
   });
@@ -149,17 +148,17 @@ namesInput.forEach((input)=>{
 // Input Quantity Competitions
 const quantityInput = document.querySelector('#quantity');
 
-quantityInput.addEventListener('change',(e) => {
+quantityInput.addEventListener('change', (e) => {
 
   if (quantityInput.value == 0) {
 
     // Display Infos in Log
-    displayInputDataLog(quantityInput,'wrong');
+    displayInputDataLog(quantityInput, 'wrong');
 
-  }  else {
+  } else {
 
-      displayInputDataLog(quantityInput);
-      
+    displayInputDataLog(quantityInput);
+
   }
 
 });
@@ -176,107 +175,107 @@ quantityInput.addEventListener('change',(e) => {
  * @param {string}  wrong
  */
 
-  // Edit Nav
-  function editNav(navItem) {
-    var x = document.getElementById(`${navitem}`);
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+// Edit Nav
+function editNav(navItem) {
+  var x = document.getElementById(`${navitem}`);
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+// Modal Display
+function setDisplayModal(target, displayValue) {
+
+  target.style.setProperty('display', displayValue);
+
+}
+
+// Change State Element in Modal 
+function switchStateElement(element) {
+
+  element.classList.toggle('on');
+
+  // console.log('switch state function');
+
+}
+
+
+function getModalHeight(target) {
+
+  let modalFlexHeight = e.target.closest(`${target}`).offsetHeight;
+
+  // console.log(modalFlexHeight);
+
+}
+
+
+function displayInputDataLog(inputElement, state) {
+
+  if (state == 'wrong') {
+
+    console.warn(`${inputElement.getAttribute('name')} - data input is not correct`);
+
+  } else {
+
+    console.log(`${inputElement.getAttribute('name')} // is a good Input Field Data 💪`);
   }
 
-  // Modal Display
-  function setDisplayModal(target,displayValue){
-
-    target.style.setProperty('display',displayValue);
-    
-  }
-
-  // Change State Element in Modal 
-  function switchStateElement(element){
-
-    element.classList.toggle('on');
-
-    // console.log('switch state function');
-
-  }
-
-  
-  function getModalHeight(target){
-
-     let modalFlexHeight = e.target.closest(`${target}`).offsetHeight;
-
-    // console.log(modalFlexHeight);
-
-  }
-  
-
-  function displayInputDataLog(inputElement,state){
-
-    if  (state == 'wrong') {
-
-      console.warn(`${inputElement.getAttribute('name')} - data input is not correct`);
-
-    } else {
-
-      console.log(`${inputElement.getAttribute('name')} // is a good Input Field Data 💪`);
-    }
-
-  }
+}
 
 
-  function testSubmit() {
+function testSubmit() {
 
-    // event.preventDefault();
+  // event.preventDefault();
 
-    const box = document.createElement("div");
+  const box = document.createElement("div");
 
-    box.classList.add('debeug');
-    document.querySelector('.content').append(box);
+  box.classList.add('debeug');
+  document.querySelector('.content').append(box);
 
 
-    //Create Date submission
-    let timer = new Date();
+  //Create Date submission
+  let timer = new Date();
 
-    //Display submission date on debeug panel
-    box.textContent = `Form Submitted at: ${timer.getDate()}/${timer.getUTCMonth()}/${timer.getFullYear()} 
+  //Display submission date on debeug panel
+  box.textContent = `Form Submitted at: ${timer.getDate()}/${timer.getUTCMonth()}/${timer.getFullYear()} 
     ${timer.getHours()}h${timer.getMinutes()}`;
 
-    console.log(localStorage);
+  console.log(localStorage);
 
-  }
-
-
-  function getDataInput(formTargeted){
-
-    // Create formdata object to store later
-    const formData = new FormData(document.querySelector(`${formTargeted}`));
-
-    console.log('Données du Form 👉',formData);
-
-    //Clone the initial object Form Data to another thing
-    const objectData = Object.assign({},formData);
-    // console.log(objectData); 
-     // => Impossible de copier l'objet !
+}
 
 
-    // Get each values of Form
-    // for (const pairs of formData.entries()){
+function getDataInput(formTargeted) {
 
-    // }
+  // Create formdata object to store later
+  const formData = new FormData(document.querySelector(`${formTargeted}`));
 
-    
-    //Store Datas in localstorage Area
-    let exportDatas = Array.from(formData);
+  console.log('Données du Form 👉', formData);
 
-    const storageFreezeName = 'the-form-count';
-  
-    localStorage.setItem(`${storageFreezeName}`,JSON.stringify(exportDatas));
+  //Clone the initial object Form Data to another thing
+  const objectData = Object.assign({}, formData);
+  // console.log(objectData); 
+  // => Impossible de copier l'objet !
 
-    // console.log(JSON.parse(localStorage.getItem(`${storageFreezeName}`)));
 
-  }
+  // Get each values of Form
+  // for (const pairs of formData.entries()){
+
+  // }
+
+
+  //Store Datas in localstorage Area
+  let exportDatas = Array.from(formData);
+
+  const storageFreezeName = 'the-form-count';
+
+  localStorage.setItem(`${storageFreezeName}`, JSON.stringify(exportDatas));
+
+  // console.log(JSON.parse(localStorage.getItem(`${storageFreezeName}`)));
+
+}
 
 
 
